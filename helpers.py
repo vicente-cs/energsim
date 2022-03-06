9cb1e930420201c89b37d9709b67812d863d3ca6
Vicente <intfermion@gmail.com>
2022-03-05 21:49:23 -0300

    update

--- helpers.py
+++ helpers.py
@@ -1,52 +1,93 @@
 from typing import List
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Consumidor:
-    def __init__(self, nome, potencia):
+    """Algo que consome energia"""
+
+    def __init__(self, nome: str, potencia: float):
         self.nome = nome
         self.potencia = potencia
-        self._consumo = 0
 
-    def calcular(self, t_horas, taxa):
-        return self.potencia * t_horas
+    def simular(self, t_dias: float, taxa_kwh: float):
+        """Simula o consumo de um período de t_dias"""
+        return {
+            "consumo": self.consumo * t_dias,
+            "custo": self.consumo * t_dias * taxa_kwh,
+        }
 
-    # TODO: Implementar stats
-    def stats(self):
-        pass
-
+    # Nesse caso, o consumo não é diretamente definido
     @property
     def consumo(self):
-        return self._consumo
+        """Consumo em kwH"""
+        return 0
+
+    # TODO: Implementar stats
+    def grafico(self, t_dias: float, taxa_kwh: float):
+        plt.style.use("fivethirtyeight")
+
+        plt.title(f"{self.nome}: relação custo/consumo")
+        plt.xlabel("Dias")
+
+        sim = self.simular(t_dias, taxa_kwh)
+
+        x1 = np.array([0, t_dias])
+        y1 = np.array([0, sim["custo"]])
+
+        x2 = np.array([0, t_dias])
+        y2 = np.array([0, sim["consumo"]])
+
+        plt.plot(x1, y1, label="Custo (R$)")
+        plt.plot(x2, y2, label="Consumo (kwH)")
+
+        plt.legend()
+
+        plt.grid(True)
+
+        plt.tight_layout()
+        plt.show()
 
     @classmethod
     def cadastrar(self):
-        nome = input("Digite o nome da entidade: ")
-        potencia = float(input("Digite a potência da entidade (Watts): "))
+        nome = input("Nome da entidade: ")
+        potencia = float(input("Potência da entidade (Watts): "))
         return self(nome, potencia)
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + ":" + str(self.__dict__)
 
 
 class Eletrodomestico(Consumidor):
-    def __init__(self, nome, potencia, h_diario):
+    def __init__(self, nome: str, potencia: float, h_diario: float):
         super().__init__(nome, potencia)
         self.h_diario = h_diario
 
     @property
     def consumo(self):
-        return self.h_diario * self.potencia
+        return self.h_diario * self.potencia / 1000
 
     @classmethod
     def cadastrar(self):
-        nome = input("Digite o nome do eletrodoméstico: ")
-        potencia = float(input("Digite a potência do eletrodoméstico (Watts): "))
-        h_diario = float(input("Digite o tempo de uso diário (Horas): "))
+        nome = input("Nome do eletrodoméstico: ")
+        potencia = float(input("Potência do eletrodoméstico (Watts): "))
+        h_diario = float(input("Tempo de uso diário (Horas): "))
         return self(nome, potencia, h_diario)
 
 
 class Residencia(Consumidor):
-    def __init__(self, nome, taxa, eletrodomesticos: List[Eletrodomestico] = []):
+    def __init__(
+        self, nome: str, taxa_kwh: float, eletrodomesticos: List[Eletrodomestico] = []
+    ):
         self.nome = nome
-        self.taxa = taxa
+        self.taxa_kwh = taxa_kwh
         self.eletrodomesticos = eletrodomesticos
+
+    def simular(self, t_dias: float, taxa_kwh: float = None):
+        if taxa_kwh == None:
+            taxa_kwh = self.taxa_kwh
+
+        return super().simular(t_dias, taxa_kwh)
 
     @property
     def consumo(self):
@@ -59,5 +100,6 @@ class Residencia(Consumidor):
 
     @classmethod
     def cadastrar(self):
-        nome = input("Digite o nome da residência: ")
-        return self(nome)
+        nome = input("Nome da residência: ")
+        taxa_kwh = float(input("Taxa de energia: "))
+        return self(nome, taxa_kwh)
