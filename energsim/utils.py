21538d2bf5920e9eecd1e40bddb6ab1da988eb3f
Vicente <intfermion@gmail.com>
2022-03-08 01:00:58 -0300

    add ValidarHorario

--- energsim/utils.py
+++ energsim/utils.py
@@ -1,12 +1,21 @@
+from pydoc import doc
 from prompt_toolkit.validation import Validator, ValidationError
 
-#TODO Implementar validação horário
+# TODO Implementar validação horário
 class ValidarNumero(Validator):
     def validate(self, document):
         try:
             float(document.text)
         except:
             raise ValidationError(message="Digite um valor válido")
-    
+
+
 class ValidarHorario(Validator):
-    pass
+    def validate(self, document):
+        message = "Digite um valor válido"
+        try:
+            val = float(document.text)
+            if not (0 <= val <= 24):
+                raise ValidationError(message=message)
+        except:
+            raise ValidationError(message=message)
